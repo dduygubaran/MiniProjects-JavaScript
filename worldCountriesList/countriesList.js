5144fb3f39fa8c47d9f31b09c9649251921b8328
dduygubaran <duygubaran1995@gmail.com>
2024-03-07 16:28:31 +0300

    sdkljf

---
+++ worldCountriesList/countriesList.js
@@ -0,0 +1,16 @@
+
+let btn = document.createElement("button")
+
+btn.style.backgroundColor = "red"
+btn.style.padding = "20px"
+btn.style.borderRadius = "10px"
+
+
+// margin-right: 10px;
+// padding: 20px;
+// background-color: #ffa82e;
+// font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+// border-radius: 10px;
+// border-color: #ffa82e;   
+
+document.body.appendChild(btn)
